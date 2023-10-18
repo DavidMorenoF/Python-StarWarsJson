f99c389b5f1abb179ba72481c5be1b8ce783a750
sjo <usuari@domini.com>
2023-10-18 18:43:41 +0200

    Done

--- character_films.py
+++ character_films.py
@@ -4,12 +4,31 @@ from character import Character
 class Character_films(Character):
     
 
-    def __init__(self, name, created, gender, skin_color, hair_color, height, eye_color, mass, homeworld, birth_year, num_of_films, first_film, alive_at_the_end):
+    def __init__(self, name, created, gender, skin_color, hair_color, height, eye_color, mass, homeworld, birth_year):
         super().__init__(name, created, gender, skin_color, hair_color, height, eye_color, mass, homeworld, birth_year)
-        self.__num_of_films = num_of_films
+        
 
+    @property
+    def num_of_films(self):
+        return self._num_of_films
     
     @num_of_films.setter
     def num_of_films(self, num):
-        self.__num_of_films = num
+        self._num_of_films = num
+
+    @property
+    def first_film(self):
+        return self._first_film
+    
+    @first_film.setter
+    def first_film(self, film):
+        self._first_film = film
+
+    @property
+    def alive_at_the_end(self):
+        return self._alive_at_the_end
+    
+    @alive_at_the_end.setter
+    def alive_at_the_end(self, boolean):
+        self._alive_at_the_end = boolean
         
--- main.py
+++ main.py
@@ -1,29 +1,77 @@
 import json
 from character import Character
+from character_films import Character_films
 
 
 def read_characters_and_add_class(path):
     character_list = []
     with open (path) as archivo:
-        datos = json.load(archivo)
-    for i in datos:
+        characters = json.load(archivo)
+    for character in characters:
         character_actual = Character(
-            i["fields"]["name"],
-            i["fields"]["created"],
-            i["fields"]["gender"],
-            i["fields"]["skin_color"], 
-            i["fields"]["hair_color"], 
-            i["fields"]["height"], 
-            i["fields"]["eye_color"], 
-            i["fields"]["mass"],
-            i["fields"]["homeworld"],
-            i["fields"]["birth_year"])   
+            character["fields"]["name"],
+            character["fields"]["created"],
+            character["fields"]["gender"],
+            character["fields"]["skin_color"], 
+            character["fields"]["hair_color"], 
+            character["fields"]["height"], 
+            character["fields"]["eye_color"], 
+            character["fields"]["mass"],
+            character["fields"]["homeworld"],
+            character["fields"]["birth_year"])   
         character_list.append(character_actual)
     
     return character_list
+
+def pasar_a_character_films(lista):
+    lista2 = []
+    for character in lista:
+        character_actual = Character_films(
+            character.name,
+            character.created,
+            character.gender,
+            character.skin_color, 
+            character.hair_color, 
+            character.height, 
+            character.eye_color, 
+            character.mass,
+            character.homeworld,
+            character.birth_year,
+            )  
+        match(character_actual.name):
+            case "Luke Skywalker":
+                character_actual.first_film = "Una Nueva Esperanza (1977)"
+                character_actual.num_of_films = 6
+                character_actual.alive_at_the_end = False
+            case "Chewbacca":
+                character_actual.first_film = "Una Nueva Esperanza (1977)"
+                character_actual.num_of_films = 8
+                character_actual.alive_at_the_end = True
+            case "Anakin Skywalker":
+                character_actual.first_film = "La Amenaza Fantasma (1999)"
+                character_actual.num_of_films = 6
+                character_actual.alive_at_the_end = False
+            case _:
+                character_actual.first_film = None
+                character_actual.num_of_films = None
+                character_actual.alive_at_the_end = None
+            
+        lista2.append(character_actual)
+    return lista2
     
+def parse_to_Json(list):
+    pasar_a_json = []
+    for i in list:
+        pasar_a_json.append(i.__dict__)
+
+    with open("StarWarsFilmsInfo.json", "w") as file:
+        json.dump(pasar_a_json, file, indent=4)
+
+
 def main():
     character_list = read_characters_and_add_class("StarWars.json")
-    character_list[0].__str__()
+    print(character_list[0].__str__())
+    character_films_list= pasar_a_character_films(character_list)
+    parse_to_Json(character_films_list)
 
 main()
